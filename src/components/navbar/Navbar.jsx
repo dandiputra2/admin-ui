0a419f5492ce36d7fa310b4483aaae0b4e39ecfc
unknown <111202113684@mhs.dinus.ac.id>
2024-04-13 01:20:37 +0700

    ubah teks

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import "./navbar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <DarkModeOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <img
+              src="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fENhdHxlbnwwfHwwfHx8MA%3D%3D"
+              alt=""
+              className="avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,28 @@
 import "./home.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
+import Chart from "../../components/chart/Chart";
 
-const home = () => {
-  
-return <div>home</div>;
-  
+const Home = () => {
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar/>
+        <div className="widgets">
+            <Widget type="user"/>
+            <Widget type="order"/>
+            <Widget type="earning"/>
+            <Widget type="balance"/>
+        </div>
+        <div className="charts">
+            <Chart type="order" aspect={2/1}/>
+            <Chart type="earning" aspect={2/1}/>
+        </div>
+        </div>
+    </div>
+  );
 };
 
-export default home;
+export default Home;
