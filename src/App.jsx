46d39c4acfd492680d967c5a29aefe50f174d928
unknown <111202113684@mhs.dinus.ac.id>
2024-05-05 00:36:44 +0700

    menambahkan view dan add new

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { productInputs,userInputs } from "./formsource";
 
 function App() {
   return (
@@ -14,11 +15,22 @@ function App() {
           <Route path="/">
             <Route index element={<Home />}></Route>
             <Route path="login" element={<Login />}></Route>
-            <Route path="products">
+            <Route path="users">
               <Route index element={<List />}></Route>
               <Route path=":userId" element={<Single />}></Route>
-              <Route path="new" element={<New />}></Route>
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add New User"/>}
+                />
             </Route>
+            <Route path="products">
+              <Route index element={<List />}></Route>
+              <Route path=":productId" element={<Single />}></Route>
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add New User"/>}
+                />
+                </Route>
           </Route>
         </Routes>
       </BrowserRouter>
